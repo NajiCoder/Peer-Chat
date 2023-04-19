5718a1fc85d3079c351103acf4c4d63a840a8715
Naji <lemiine987@gmail.com>
2023-04-19 13:52:30 +0300

    Make peerconnection with ice candidates

--- main.js
+++ main.js
@@ -5,11 +5,10 @@ let peerConnection;
 const servers = {
   iceServers: [
     {
-      urls: ["stun.l.google.com:19302", "stun1.l.google.com:19302"],
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
     },
   ],
 };
-
 let init = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
@@ -21,10 +20,29 @@ let init = async () => {
 };
 
 let createOffer = async () => {
-  peerConnection = new RTCPeerConnection();
+  peerConnection = new RTCPeerConnection(servers);
 
   remoteStream = new MediaStream();
   document.getElementById("user-2").srcObject = remoteStream;
+
+  // Add the local stream to the peer connection
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  // Add the remote stream to the peer connection
+  peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  // Add the ICE candidate to the peer connection
+  peerConnection.onicecandidate = async (event) => {
+    if (event.candidate) {
+      console.log("New ICE candidate : ", event.candidate);
+    }
+  };
 
   let offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
